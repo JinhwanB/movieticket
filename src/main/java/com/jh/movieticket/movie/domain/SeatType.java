64e6b48890d7ff22e0c90bd9dad1b5fd677c2393
jinhwan Bea <paewhal20@gmail.com>
2024-06-21 20:59:23 +0900

    Feat: 좌석 상태 타입을 나타내는 enum 클래스 생성

---
+++ src/main/java/com/jh/movieticket/movie/domain/SeatType.java
@@ -0,0 +1,14 @@
+package com.jh.movieticket.movie.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// 좌석 상태 enum 클래스
+@Getter
+@AllArgsConstructor
+public enum SeatType {
+    AVAILABLE("AVAILABLE"), // 예약 가능한 상태
+    BOOKED("BOOKED"); // 예약 완료된 상태
+
+    private final String name;
+}
