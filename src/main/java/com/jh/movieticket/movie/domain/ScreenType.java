0730f821a520803c4bf07e78205094fc65f59042
jinhwan Bea <paewhal20@gmail.com>
2024-06-21 20:58:56 +0900

    Feat: 영화가 상영중인지 판별하는 enum 클래스 생성

---
+++ src/main/java/com/jh/movieticket/movie/domain/ScreenType.java
@@ -0,0 +1,15 @@
+package com.jh.movieticket.movie.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+// 영화의 현재 상영 여부를 나타내는 enum 클래스
+@Getter
+@AllArgsConstructor
+public enum ScreenType {
+    NOW("NOW"), // 현재상영작
+    PREVIOUS("PREVIOUS"), // 지난 상영작
+    EXPECTED("EXPECTED"); // 상영 예정작
+
+    private final String name;
+}
