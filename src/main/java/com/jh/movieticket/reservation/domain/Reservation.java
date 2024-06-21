e36f664aca533cdf402302a382265c0be83306ef
jinhwan Bea <paewhal20@gmail.com>
2024-06-21 20:58:25 +0900

    Feat: 예약 엔티티 생성

---
+++ src/main/java/com/jh/movieticket/reservation/domain/Reservation.java
@@ -0,0 +1,59 @@
+package com.jh.movieticket.reservation.domain;
+
+import com.jh.movieticket.config.BaseTimeEntity;
+import com.jh.movieticket.member.domain.Member;
+import com.jh.movieticket.movie.domain.MovieSchedule;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+@Table(
+    name = "reservation",
+    uniqueConstraints = {
+        @UniqueConstraint(
+            name = "reservationUnique",
+            columnNames = {"reservation_number", "del_date"}
+        )
+    }
+)
+public class Reservation extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String reservationNumber; // 예약 번호
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member; // 예약한 회원
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "movie_schedule_id", nullable = false)
+    private MovieSchedule movieSchedule; // 예약한 영화의 스케줄
+
+    @Column
+    private int seatNo; // 좌석 번호
+
+    @Column
+    private LocalDateTime delDate; // 삭제날짜
+}
