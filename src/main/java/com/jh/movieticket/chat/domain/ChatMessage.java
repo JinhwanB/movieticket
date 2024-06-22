28e35c3ba8c381944f599730914d56518e1285cc
jinhwan Bea <paewhal20@gmail.com>
2024-06-22 19:29:45 +0900

    Feat: 채팅방과 채팅메시지 엔티티 생성

---
+++ src/main/java/com/jh/movieticket/chat/domain/ChatMessage.java
@@ -0,0 +1,43 @@
+package com.jh.movieticket.chat.domain;
+
+import com.jh.movieticket.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom; // 채팅방
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "sender_id", nullable = false)
+    private Member sender; // 보낸 사람
+
+    @Column(nullable = false, length = 5000)
+    private String message; // 메시지 내용
+
+    @Column
+    private LocalDateTime delDate; // 삭제날짜
+}
---
+++ src/main/java/com/jh/movieticket/chat/domain/ChatRoom.java
@@ -0,0 +1,41 @@
+package com.jh.movieticket.chat.domain;
+
+import com.jh.movieticket.config.BaseTimeEntity;
+import com.jh.movieticket.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member1_id", nullable = false)
+    private Member admin; // 관리자
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member2_id", nullable = false)
+    private Member member; // 회원
+
+    @Column
+    private LocalDateTime delDate; // 삭제날짜
+}
