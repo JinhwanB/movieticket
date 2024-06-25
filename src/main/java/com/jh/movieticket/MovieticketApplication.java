05bcdfa1388b9a954a96a3ef42dfee9e27d0b634
jinhwan Bea <paewhal20@gmail.com>
2024-06-25 15:44:04 +0900

    Refactor: 캐싱 기능 활성화

--- src/main/java/com/jh/movieticket/MovieticketApplication.java
+++ src/main/java/com/jh/movieticket/MovieticketApplication.java
@@ -2,10 +2,12 @@ package com.jh.movieticket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement // transactionManagement 활성화
+@EnableCaching // 캐싱 기능 활성화
 public class MovieticketApplication {
 
 	public static void main(String[] args) {
