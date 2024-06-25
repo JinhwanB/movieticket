8c0fef737c168125f9b5bf57305fba4bd0fc8bc3
jinhwan Bea <paewhal20@gmail.com>
2024-06-25 23:12:54 +0900

    Feat: 회원 엔티티를 로그인 dto의 response로 변경하는 메소드 추가

--- src/main/java/com/jh/movieticket/member/domain/Member.java
+++ src/main/java/com/jh/movieticket/member/domain/Member.java
@@ -1,6 +1,7 @@
 package com.jh.movieticket.member.domain;
 
 import com.jh.movieticket.config.BaseTimeEntity;
+import com.jh.movieticket.member.dto.MemberSignInDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -11,6 +12,7 @@ import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -65,5 +67,18 @@ public class Member extends BaseTimeEntity implements UserDetails {
     public String getUsername() {
 
         return userId;
+    }
+
+    /**
+     * Entity -> LoginResponse
+     *
+     * @return LoginResponse
+     */
+    public MemberSignInDto.Response toLoginResponse() {
+
+        return MemberSignInDto.Response.builder()
+            .userId(userId)
+            .role(List.of(role.getName()))
+            .build();
     }
 }
