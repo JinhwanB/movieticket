c41807666e22047857de51215308c3b99b2b645d
jinhwan Bea <paewhal20@gmail.com>
2024-06-25 16:39:33 +0900

    Feat: 비밀번호가 다른 경우 에러코드 추가

--- src/main/java/com/jh/movieticket/member/exception/MemberErrorCode.java
+++ src/main/java/com/jh/movieticket/member/exception/MemberErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum MemberErrorCode {
+    NOT_MATCH_PASSWORD(400, "비밀번호가 다릅니다."),
     EXIST_USER_ID(400, "이미 사용중인 아이디입니다."),
     EXIST_EMAIL(400, "이메일로 등록된 회원이 있습니다."),
     NOT_FOUND_MEMBER(400, "회원으로 등록된 유저가 아닙니다.");
