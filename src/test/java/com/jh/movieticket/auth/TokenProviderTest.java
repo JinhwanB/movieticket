f2d064e0ee8e0ce8537175c7d26aa481a9605e2e
jinhwan Bea <paewhal20@gmail.com>
2024-06-27 00:35:25 +0900

    Test: accessToken 재발급 테스트

--- src/test/java/com/jh/movieticket/auth/TokenProviderTest.java
+++ src/test/java/com/jh/movieticket/auth/TokenProviderTest.java
@@ -1,6 +1,7 @@
 package com.jh.movieticket.auth;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.jh.movieticket.member.service.MemberService;
 import jakarta.servlet.http.Cookie;
@@ -14,6 +15,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.util.ReflectionUtils;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -66,5 +68,37 @@ class TokenProviderTest {
             .orElse(null);
 
         assertThat(cookie).isNotNull();
+    }
+
+    @Test
+    @DisplayName("access 토큰 재발급")
+    void reGenerate(){
+
+        String userName = "test";
+        List<String> roles = List.of("USER");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        tokenProvider.generateRefreshToken(userName, roles, response);
+
+        request.setCookies(response.getCookies());
+        String newAccessToken = tokenProvider.reGenerateAccessToken(request, response);
+
+        assertThat(newAccessToken).isNotNull();
+    }
+
+    @Test
+    @DisplayName("access 토큰 재발급 실패 - 쿠키 없음")
+    void reGenerateFail1(){
+
+        String userName = "test";
+        List<String> roles = List.of("USER");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        tokenProvider.generateRefreshToken(userName, roles, response);
+
+        assertThatThrownBy(() -> tokenProvider.reGenerateAccessToken(request, response)).isInstanceOf(
+            TokenException.class);
     }
 }
