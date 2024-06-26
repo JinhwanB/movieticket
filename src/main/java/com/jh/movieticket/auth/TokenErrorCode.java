0aecd48cc2a6db85c7263d854c2f0ea07fdcf24a
jinhwan Bea <paewhal20@gmail.com>
2024-06-26 15:03:05 +0900

    Feat: 쿠키에 리프레시 토큰이 없는 경우 에러코드 추가

--- src/main/java/com/jh/movieticket/auth/TokenErrorCode.java
+++ src/main/java/com/jh/movieticket/auth/TokenErrorCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum TokenErrorCode {
-    EXPIRED_REFRESH_TOKEN(400, "유효하지 않은 refreshToken입니다. 재로그인이 필요합니다."),
+    NOT_FOUND_REFRESH_TOKEN(400, "refreshToken이 없습니다. 재로그인이 필요합니다."),
+    EXPIRED_REFRESH_TOKEN(400, "만료된 refreshToken입니다. 재로그인이 필요합니다."),
     EXPIRED_TOKEN(400, "요청한 토큰은 만료된 토큰입니다. 재발급이 필요합니다.");
 
     private final int status;
