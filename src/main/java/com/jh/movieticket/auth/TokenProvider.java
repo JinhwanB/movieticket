3b62d2f35ab6ea174c332c03c0b7a2e136747540
jinhwan Bea <paewhal20@gmail.com>
2024-06-26 13:47:43 +0900

    Refactor: 토큰 생성 및 토큰 정보 가져오는 로직 수정
    리프레시 토큰에 claim 생성
    공통된 부분 메소드화
    토큰 정보 가져올 때 만료된 토큰인 경우 예외 던지도록 수정

--- src/main/java/com/jh/movieticket/auth/TokenProvider.java
+++ src/main/java/com/jh/movieticket/auth/TokenProvider.java
@@ -38,49 +38,34 @@ public class TokenProvider {
 
     // access 토큰 생성 발급
     public String generateAccessToken(String userName, List<String> roles) {
-        Claims claims = Jwts.claims().setSubject(userName);
-        claims.put(KEY_ROLES, roles);
 
-        Date now = new Date();
-        Date expireDate = new Date(now.getTime() + ACCESS_TOKEN_EXPIRE_TIME);
-
-        return Jwts.builder()
-            .setClaims(claims)
-            .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-            .setIssuedAt(now) // 토큰 생성 시간
-            .setExpiration(expireDate) // 토큰 만료 시간
-            .signWith(SignatureAlgorithm.HS512, secret) // 사용할 암호화 알고리즘, 비밀키
-            .compact();
+        return createToken(userName, roles, ACCESS_TOKEN_EXPIRE_TIME);
     }
 
     // refresh 토큰 생성 발급
-    public String generateRefreshToken(String userName) {
-        Date now = new Date();
-        Date expireDate = new Date(now.getTime() + REFRESH_TOKEN_EXPIRE_TIME);
+    public String generateRefreshToken(String userName, List<String> roles) {
 
-        return Jwts.builder()
-            .setSubject(userName)
-            .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
-            .setIssuedAt(now) // 토큰 생성 시간
-            .setExpiration(expireDate) // 토큰 만료 시간
-            .signWith(SignatureAlgorithm.HS512, secret) // 사용할 암호화 알고리즘, 비밀키
-            .compact();
+        return createToken(userName, roles, REFRESH_TOKEN_EXPIRE_TIME);
     }
 
     // jwt를 사용하여 사용자의 인증 정보 가져오는 메소드
     @Transactional
     public Authentication getAuthentication(String jwt) {
+
         UserDetails userDetails = memberService.loadUserByUsername(getUserName(jwt));
+
         return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
     // 유저 아이디 가져오기
     public String getUserName(String token) {
+
         return parseClaims(token).getSubject();
     }
 
     // 헤더 정보의 토큰을 가져온다.
     public String resolveTokenFromRequest(HttpServletRequest request) {
+
         String token = request.getHeader(TOKEN_HEADER);
 
         if (!ObjectUtils.isEmpty(token) && token.startsWith(TOKEN_PREFIX)) {
@@ -92,19 +77,39 @@ public class TokenProvider {
 
     // 토큰 검증
     public boolean validateToken(String token) {
+
         if (!StringUtils.hasText(token)) return false;
 
         Claims claims = parseClaims(token);
+
         return !claims.getExpiration().before(new Date());
     }
 
     // 토큰 정보 가져오기
     private Claims parseClaims(String token) {
+
         try {
             return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
         } catch (ExpiredJwtException e) {
             log.error("토큰 정보 에러 = {}", e.getMessage());
-            return e.getClaims();
+            throw new TokenException(TokenErrorCode.EXPIRED_TOKEN);
         }
+    }
+
+    private String createToken(String userName, List<String> role, long tokenExpiredTime){
+
+        Claims claims = Jwts.claims().setSubject(userName);
+        claims.put(KEY_ROLES, role);
+
+        Date now = new Date();
+        Date expireDate = new Date(now.getTime() + tokenExpiredTime);
+
+        return Jwts.builder()
+            .setClaims(claims)
+            .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+            .setIssuedAt(now) // 토큰 생성 시간
+            .setExpiration(expireDate) // 토큰 만료 시간
+            .signWith(SignatureAlgorithm.HS512, secret) // 사용할 암호화 알고리즘, 비밀키
+            .compact();
     }
 }
