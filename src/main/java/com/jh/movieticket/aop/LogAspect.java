4b524fe87a46abe5753100767edfd58f50528035
jinhwan Bea <paewhal20@gmail.com>
2024-06-27 13:44:53 +0900

    Feat: 로그 작성 aop
    컨트롤러 및 서비스 로그 작성 및 예외 발생 로그 작성

---
+++ src/main/java/com/jh/movieticket/aop/LogAspect.java
@@ -0,0 +1,77 @@
+package com.jh.movieticket.aop;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Slf4j
+@Component
+public class LogAspect {
+
+    // 모든 메소드 포인트컷
+    @Pointcut("execution(* com.jh.movieticket..*(..)) " + "&& !execution(* com.jh.movieticket.auth..*(..)) " + "&& !execution(* com.jh.movieticket.config..*(..)) " + "&& !execution(* com.jh.movieticket.validation..*(..))")
+    public void all() {
+    }
+
+    // Controller클래스의 모든 메소드 포인트컷
+    @Pointcut("execution(* com.jh.movieticket.*Controller..*(..))")
+    public void controller() {
+    }
+
+    // Service클래스의 모든 메소드 포인트컷
+    @Pointcut("execution(* com.jh.movieticket.*Service..*(..))")
+    public void service() {
+    }
+
+    // Service 또는 Controller의 메소드 실행 전 로그 표시
+    @Before("controller() || service()")
+    public void beforeLog(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        Method method = signature.getMethod();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        log.info("[{}] {}메소드 실행", className, method.getName());
+
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg != null) {
+                if (arg.getClass().getPackage().getName().startsWith("com.jh.restaurantreservationpj")) {
+                    log.info("파라미터 타입 = {}", arg.getClass().getName());
+
+                    // 객체 파라미터 필드 값 로그 출력
+                    Field[] fields = arg.getClass().getDeclaredFields();
+                    for (Field field : fields) {
+                        field.setAccessible(true);  // private 필드 접근 허용
+                        try {
+                            Object fieldValue = field.get(arg);
+                            log.info("필드 이름 = {} -> {}", field.getName(), fieldValue);
+                        } catch (IllegalAccessException e) {
+                            log.error("필드 값을 가져올 수 없음", e);
+                        }
+                    }
+
+                    continue;
+                }
+
+                log.info("파라미터 타입 = {} -> {}", arg.getClass().getSimpleName(), arg);
+            }
+        }
+    }
+
+    // 모든 메소드에서 예외 발생시 로그 표시
+    @AfterThrowing(pointcut = "all()", throwing = "ex")
+    public void afterThrowingLog(JoinPoint joinPoint, Throwable ex) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        String className = joinPoint.getTarget().getClass().getSimpleName();
+        log.error("[{}] {}메소드에서 예외 발생", className, method.getName());
+        log.error("Exception message = {}", ex.getMessage());
+    }
+}
