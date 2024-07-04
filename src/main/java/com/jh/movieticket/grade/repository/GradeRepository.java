d33aa7cf75fca3aaf21e2da2868a8c32a5682f0c
jinhwan Bea <paewhal20@gmail.com>
2024-07-04 17:01:07 +0900

    Feat: 평점 레포지토리 생성
    회원 소프트딜리트로 인한 평점 엔티티 삭제 메소드 추가

---
+++ src/main/java/com/jh/movieticket/grade/repository/GradeRepository.java
@@ -0,0 +1,17 @@
+package com.jh.movieticket.grade.repository;
+
+import com.jh.movieticket.grade.domain.Grade;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public interface GradeRepository extends JpaRepository<Grade, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE Grade g WHERE g.member.id = :parentId")
+    void deleteGradeByMember(Long parentId); // 회원 소프트딜리트로 인한 평점 엔티티 삭제 메소드
+}
