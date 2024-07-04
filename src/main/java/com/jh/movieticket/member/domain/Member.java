b1ad6bf1e60bf9a985cc775bb3bbbc6af43f4781
jinhwan Bea <paewhal20@gmail.com>
2024-07-04 17:46:48 +0900

    Refactor: 단방향 맵핑

--- src/main/java/com/jh/movieticket/member/domain/Member.java
+++ src/main/java/com/jh/movieticket/member/domain/Member.java
@@ -1,10 +1,7 @@
 package com.jh.movieticket.member.domain;
 
-import com.jh.movieticket.chat.domain.ChatRoom;
 import com.jh.movieticket.config.BaseTimeEntity;
-import com.jh.movieticket.grade.domain.Grade;
 import com.jh.movieticket.member.dto.MemberServiceDto;
-import com.jh.movieticket.reservation.domain.Reservation;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,9 +9,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -47,15 +42,6 @@ public class Member extends BaseTimeEntity {
 
     @Column
     private LocalDateTime deleteDate; // 삭제날짜
-
-    @OneToMany(mappedBy = "member")
-    private List<ChatRoom> chatRoomList; // 채팅 메시지
-
-    @OneToMany(mappedBy = "member")
-    private List<Grade> gradeList; // 평점
-
-    @OneToMany(mappedBy = "member")
-    private List<Reservation> reservationList; // 예매
 
     /**
      * Entity -> ServiceDto
