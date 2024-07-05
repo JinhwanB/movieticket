b2609609d33094dc85c2d95d47be9c5537de3aa3
jinhwan Bea <paewhal20@gmail.com>
2024-07-05 15:17:31 +0900

    Feat: 상영관 이름 중복일 시 에러코드 추가

--- src/main/java/com/jh/movieticket/theater/exception/TheaterErrorCode.java
+++ src/main/java/com/jh/movieticket/theater/exception/TheaterErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum TheaterErrorCode {
 
+    EXIST_NAME(HttpStatus.BAD_REQUEST.value(), "중복된 상영관 이름입니다."),
     NOT_FOUND_THEATER(HttpStatus.BAD_REQUEST.value(), "등록되지 않은 상영관입니다.");
 
     private final int status;
