67642e07400067c52c5be674841d5dd0cb3ee18e
jinhwan Bea <paewhal20@gmail.com>
2024-07-05 14:08:14 +0900

    Feat: 서비스에서 사용할 dto

---
+++ src/main/java/com/jh/movieticket/theater/dto/TheaterServiceDto.java
@@ -0,0 +1,23 @@
+package com.jh.movieticket.theater.dto;
+
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 서비스 레이어에서 사용할 dto
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class TheaterServiceDto {
+
+    private Long id;
+    private String name; // 상영관 이름
+    private int seatCnt; // 총 좌석 수
+    private LocalDateTime registerDate; // 생성 날짜
+    private LocalDateTime changeDate; // 수정 날짜
+    private LocalDateTime deleteDate; // 삭제 날짜
+}
