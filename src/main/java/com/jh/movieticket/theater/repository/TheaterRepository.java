ddcc6056ff4983f07727338bedb366637d10dc8d
jinhwan Bea <paewhal20@gmail.com>
2024-07-05 15:31:16 +0900

    Feat: 상영관 이름이로 상영관 조회하는 쿼리 메소드 추가

--- src/main/java/com/jh/movieticket/theater/repository/TheaterRepository.java
+++ src/main/java/com/jh/movieticket/theater/repository/TheaterRepository.java
@@ -2,6 +2,7 @@ package com.jh.movieticket.theater.repository;
 
 import com.jh.movieticket.theater.domain.Theater;
 import java.time.LocalDateTime;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface TheaterRepository extends JpaRepository<Theater, Long> {
 
     boolean existsByNameAndDeleteDate(String name, LocalDateTime deleteDate); // 상영관 이름 중복 여부 확인 메소드
+
+    Optional<Theater> findByNameAndDeleteDate(String name,
+        LocalDateTime deleteDate); // 상영관 이름으로 상영관 조회
 }
