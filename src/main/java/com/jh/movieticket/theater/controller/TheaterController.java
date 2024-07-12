9083d82e0b71f58bad27350999b16525caf3b5cb
jinhwan Bea <paewhal20@gmail.com>
2024-07-12 19:51:59 +0900

    Feat: 상영관 생성 컨트롤러 구현

---
+++ src/main/java/com/jh/movieticket/theater/controller/TheaterController.java
@@ -0,0 +1,57 @@
+package com.jh.movieticket.theater.controller;
+
+import com.jh.movieticket.config.GlobalApiResponse;
+import com.jh.movieticket.theater.dto.TheaterCreateDto;
+import com.jh.movieticket.theater.dto.TheaterModifyDto;
+import com.jh.movieticket.theater.dto.TheaterServiceDto;
+import com.jh.movieticket.theater.dto.TheaterVerifyDto;
+import com.jh.movieticket.theater.dto.TheaterVerifyDto.Response;
+import com.jh.movieticket.theater.service.TheaterService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/theaters")
+@RequiredArgsConstructor
+@Validated
+public class TheaterController {
+
+    private final TheaterService theaterService;
+
+    /**
+     * 상영관 생성 컨트롤러
+     *
+     * @param request 상영관 생성 정보
+     * @return 성공 시 201 코드와 생성된 상영관, 실패 시 에러코드와 에러메시지
+     */
+    @PostMapping("/theater")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<GlobalApiResponse<TheaterCreateDto.Response>> createOfTheater(
+        @Valid @RequestBody TheaterCreateDto.Request request) {
+
+        TheaterServiceDto theater = theaterService.createTheater(request);
+
+        return ResponseEntity.ok(
+            GlobalApiResponse.toGlobalResponse(HttpStatus.CREATED, theater.toCreateResponse()));
+    }
+
