03a65a92ded303135dd500f6da678219c31fa139
jinhwan Bea <paewhal20@gmail.com>
2024-07-12 14:23:07 +0900

    Feat: 상영관 캐시 이름 추가

--- src/main/java/com/jh/movieticket/config/CacheName.java
+++ src/main/java/com/jh/movieticket/config/CacheName.java
@@ -3,4 +3,5 @@ package com.jh.movieticket.config;
 public class CacheName {
 
     public static final String MEMBER_CACHE_NAME = "member";
+    public static final String THEATER_CACHE_NAME = "theater";
 }
