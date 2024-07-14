17252c95d3a183257e9fda4188800d2a2fb5ac5e
jinhwan Bea <paewhal20@gmail.com>
2024-07-15 02:26:27 +0900

    Feat: 삭제되지 않은 데이터 중 이름으로 조회하는 쿼리 메소드 추가

--- src/main/java/com/jh/movieticket/movie/repository/MovieRepository.java
+++ src/main/java/com/jh/movieticket/movie/repository/MovieRepository.java
@@ -1,10 +1,12 @@
 package com.jh.movieticket.movie.repository;
 
 import com.jh.movieticket.movie.domain.Movie;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
+    Optional<Movie> findByTitleAndDeleteDateIsNull(String title); // 삭제되지 않은 데이터 중 이름으로 조회
 }
