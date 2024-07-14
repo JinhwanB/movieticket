03a1904e8d9dbf6d7a4588d6b468b78f4f1d1ebf
jinhwan Bea <paewhal20@gmail.com>
2024-07-15 07:46:28 +0900

    Feat: 상영 타입 유효성 검증을 위한 코드 추가

--- src/main/java/com/jh/movieticket/movie/domain/ScreenType.java
+++ src/main/java/com/jh/movieticket/movie/domain/ScreenType.java
@@ -1,5 +1,7 @@
 package com.jh.movieticket.movie.domain;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.Arrays;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,4 +15,14 @@ public enum ScreenType {
 
     private final String name;
     private final String description;
+
+    // Enum 검증을 위한 코드, Enum에 속하지 않으면 null 리턴
+    @JsonCreator
+    private static ScreenType fromScreenType(String value) {
+
+        return Arrays.stream(ScreenType.values())
+            .filter(r -> r.getName().equals(value.toUpperCase()))
+            .findAny()
+            .orElse(null);
+    }
 }
