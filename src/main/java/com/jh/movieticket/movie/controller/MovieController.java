a2fedffeffc42eff384586effdd8ea09fc40c347
jinhwan Bea <paewhal20@gmail.com>
2024-07-15 02:12:19 +0900

    Feat: 영화 컨트롤러 생성

---
+++ src/main/java/com/jh/movieticket/movie/controller/MovieController.java
@@ -0,0 +1,12 @@
+package com.jh.movieticket.movie.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+}
