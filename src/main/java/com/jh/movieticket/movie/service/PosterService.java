0b93ae75d93c2a1a0281d302ff1768faacff0592
jinhwan Bea <paewhal20@gmail.com>
2024-07-15 13:21:53 +0900

    Feat: s3에 저장된 이미지 삭제 기능 구현

--- src/main/java/com/jh/movieticket/movie/service/PosterService.java
+++ src/main/java/com/jh/movieticket/movie/service/PosterService.java
@@ -28,17 +28,18 @@ public class PosterService {
 
     /**
      * 이미지를 s3에 저장하고 로컬에 저장된 이미지를 제거
+     *
      * @param image 저장할 이미지
      * @return s3에 저장된 이미지 링크
      */
-    public String upload(MultipartFile image){
+    public String upload(MultipartFile image) {
         File uploadFile = convertToFile(image)
             .orElseThrow(() -> new PosterException(PosterErrorCode.FAIL_CONVERT_TO_FILE));
 
         String imageName = UUID.randomUUID() + uploadFile.getName(); // s3에 저장할 이미지 이름
         String imageUrl = uploadToS3(uploadFile, imageName);
 
-        if(!uploadFile.delete()){ // 로컬에 저장되어 있는 이미지 제거
+        if (!uploadFile.delete()) { // 로컬에 저장되어 있는 이미지 제거
             log.info("로컬에 저장한 이미지를 삭제 실패했습니다. 이미지 이름 : {}", uploadFile.getName());
         }
 
@@ -46,12 +47,27 @@ public class PosterService {
     }
 
     /**
+     * s3에 저장된 이미지 삭제
+     *
+     * @param fileName 이미지 파일 이름
+     */
+    public void deleteImage(String fileName) {
+
+        try {
+            amazonS3Client.deleteObject(bucket, fileName);
+        } catch (Exception e) {
+            throw new PosterException(PosterErrorCode.FAIL_DELETE_IMAGE);
+        }
+    }
+
+    /**
      * s3에 이미지를 저장
+     *
      * @param uploadFile 저장할 이미지
-     * @param imageName 이미지 이름
+     * @param imageName  이미지 이름
      * @return 저장된 이미지 링크
      */
-    private String uploadToS3(File uploadFile, String imageName){
+    private String uploadToS3(File uploadFile, String imageName) {
 
         amazonS3Client.putObject(new PutObjectRequest(bucket, imageName, uploadFile).withCannedAcl(
             CannedAccessControlList.PublicRead));
@@ -61,22 +77,24 @@ public class PosterService {
 
     /**
      * 이미지를 로컬에 저장
+     *
      * @param image 입력받은 이미지
      * @return 로컬에 저장한 이미지
      */
-    private Optional<File> convertToFile(MultipartFile image){
+    private Optional<File> convertToFile(MultipartFile image) {
 
-        File convertFile = new File(System.getProperty("user.dir") + "/" + image.getOriginalFilename());
-        try{
+        File convertFile = new File(
+            System.getProperty("user.dir") + "/" + image.getOriginalFilename());
+        try {
             // 이미지를 위 위치에 저장
-            if(convertFile.createNewFile()){
-                try(FileOutputStream fos = new FileOutputStream(convertFile)){
+            if (convertFile.createNewFile()) {
+                try (FileOutputStream fos = new FileOutputStream(convertFile)) {
                     fos.write(image.getBytes());
                 }
 
                 return Optional.of(convertFile);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             throw new PosterException(PosterErrorCode.FAIL_CONVERT_TO_FILE);
         }
 
