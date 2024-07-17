605040b955892a8886fe8bcfd8a41671e976758d
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-17 15:50:29 +0900

    Feat: 채팅방 레포지토리 메소드 추가
    회원 엔티티를 통해 채팅방 조회하는 쿼리메소드 추가

--- src/main/java/com/jh/movieticket/chat/repository/ChatRoomRepository.java
+++ src/main/java/com/jh/movieticket/chat/repository/ChatRoomRepository.java
@@ -1,6 +1,8 @@
 package com.jh.movieticket.chat.repository;
 
 import com.jh.movieticket.chat.domain.ChatRoom;
+import com.jh.movieticket.member.domain.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,6 @@ public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     @Modifying
     @Query("DELETE ChatRoom cr WHERE cr.member.id = :parentId")
     void deleteChatRoomByMember(Long parentId); // 회원 소프트딜리트로 인한 삭제 메소드
+
+    Optional<ChatRoom> findByMember(Member member); // 회원을 통해 조회
 }
