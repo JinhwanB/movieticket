f1c3f688d36df153da95811deda19c2ac34cab48
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-17 15:48:40 +0900

    Feat: 채팅메시지 서비스 레이어 dto 생성

---
+++ src/main/java/com/jh/movieticket/chat/dto/ChatMessageServiceDto.java
@@ -0,0 +1,20 @@
+package com.jh.movieticket.chat.dto;
+
+import java.io.Serializable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+// 채팅메시지 서비스 레이어 dto
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(toBuilder = true)
+public class ChatMessageServiceDto implements Serializable {
+
+    private Long id; // pk
+    private String senderId; // 메시지 보낸 회원 아이디
+    private String message; // 보낸 메시지
+}
