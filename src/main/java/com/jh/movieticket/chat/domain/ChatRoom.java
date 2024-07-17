be4ecff85e942b408bdcb5834f93c5f316201853
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-17 15:47:39 +0900

    Feat: 채팅방 엔티티 필드 추가
    채팅 메시지 필드 추가

--- src/main/java/com/jh/movieticket/chat/domain/ChatRoom.java
+++ src/main/java/com/jh/movieticket/chat/domain/ChatRoom.java
@@ -10,12 +10,16 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.SQLRestriction;
 
 @Entity
@@ -37,6 +41,10 @@ public class ChatRoom extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member; // 회원
+
+    @BatchSize(size = 100)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "chatRoom", orphanRemoval = true)
+    private List<ChatMessage> chatMessageList; // 채팅메시지
 
     @Column
     private LocalDateTime deleteDate; // 삭제날짜
