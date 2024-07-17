59f8d80005cdb114c1eb85845f6eb5999cee70c3
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-17 13:44:14 +0900

    Feat: 영화 검색 querydsl 구현

--- src/main/java/com/jh/movieticket/movie/repository/MovieRepositoryCustom.java
+++ src/main/java/com/jh/movieticket/movie/repository/MovieRepositoryCustom.java
@@ -1,11 +1,11 @@
 package com.jh.movieticket.movie.repository;
 
 import com.jh.movieticket.movie.domain.Movie;
-import com.jh.movieticket.movie.domain.ScreenType;
+import com.jh.movieticket.movie.dto.MovieSearchDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface MovieRepositoryCustom {
 
-    Page<Movie> findBySearchOption(String title, ScreenType screenType, String genre, String orderBy, Pageable pageable);
+    Page<Movie> findBySearchOption(MovieSearchDto.Request searchRequest, Pageable pageable);
 }
--- src/main/java/com/jh/movieticket/movie/repository/MovieRepositoryCustomImpl.java
+++ src/main/java/com/jh/movieticket/movie/repository/MovieRepositoryCustomImpl.java
@@ -6,6 +6,7 @@ import static com.jh.movieticket.movie.domain.QMovieGenre.movieGenre;
 
 import com.jh.movieticket.movie.domain.Movie;
 import com.jh.movieticket.movie.domain.ScreenType;
+import com.jh.movieticket.movie.dto.MovieSearchDto;
 import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -20,19 +21,20 @@ public class MovieRepositoryCustomImpl implements MovieRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
 
-    public MovieRepositoryCustomImpl(EntityManager entityManager){
+    public MovieRepositoryCustomImpl(EntityManager entityManager) {
         this.jpaQueryFactory = new JPAQueryFactory(entityManager);
     }
 
     @Override
-    public Page<Movie> findBySearchOption(String title, ScreenType screenType, String genre,
-        String orderBy, Pageable pageable) {
+    public Page<Movie> findBySearchOption(MovieSearchDto.Request searchRequest, Pageable pageable) {
 
         List<Movie> movies = jpaQueryFactory.selectFrom(movie)
             .leftJoin(movie.movieGenreList, movieGenre)
             .leftJoin(movie.movieActorList, movieActor)
-            .where(containsTitle(title), eqScreenType(screenType), eqGenre(genre), movie.deleteDate.isNull())
-            .orderBy(getOrderSpecifier(orderBy))
+            .where(containsTitle(searchRequest.getTitle()),
+                eqScreenType(searchRequest.getScreenType()), eqGenre(searchRequest.getGenre()),
+                movie.deleteDate.isNull())
+            .orderBy(getOrderSpecifier(searchRequest.getOrderBy()))
             .offset(pageable.getOffset())
             .limit(pageable.getPageSize())
             .fetch();
@@ -40,34 +42,34 @@ public class MovieRepositoryCustomImpl implements MovieRepositoryCustom {
         return new PageImpl<>(movies, pageable, movies.size());
     }
 
-    private BooleanExpression containsTitle(String title){
+    private BooleanExpression containsTitle(String title) {
 
-        if(!StringUtils.hasText(title)){
+        if (!StringUtils.hasText(title)) {
             return null;
         }
 
         return movie.title.containsIgnoreCase(title);
     }
 
-    private BooleanExpression eqScreenType(ScreenType screenType){
+    private BooleanExpression eqScreenType(ScreenType screenType) {
 
-        if(screenType == null){
+        if (screenType == null) {
             return null;
         }
 
         return movie.screenType.eq(screenType);
     }
 
-    private BooleanExpression eqGenre(String genre){
+    private BooleanExpression eqGenre(String genre) {
 
-        if(!StringUtils.hasText(genre)){
+        if (!StringUtils.hasText(genre)) {
             return null;
         }
 
         return movieGenre.genre.name.eq(genre);
     }
 
-    private OrderSpecifier<?> getOrderSpecifier(String orderBy){
+    private OrderSpecifier<?> getOrderSpecifier(String orderBy) {
 
         final String GRADE = "grade";
         final String RESERVATION = "reservation";
