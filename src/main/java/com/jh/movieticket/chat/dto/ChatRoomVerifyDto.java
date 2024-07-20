ac1dd3a2fa02d1e3b83ce17f5d45f5801b88598f
jinhwan Bea <paewhal20@gmail.com>
2024-07-20 19:34:58 +0900

    Refactor: 채팅방 조회 dto 수정
    request - 조회할 채팅방의 회원 아이디, 조회하는 회원의 아이디
    response - 안읽은 메시지 갯수 추가

--- src/main/java/com/jh/movieticket/chat/dto/ChatRoomVerifyDto.java
+++ src/main/java/com/jh/movieticket/chat/dto/ChatRoomVerifyDto.java
@@ -1,7 +1,6 @@
 package com.jh.movieticket.chat.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,11 +16,11 @@ public class ChatRoomVerifyDto {
     @Builder(toBuilder = true)
     public static class Request {
 
-        @NotBlank(message = "회원 아이디를 입력해주세요.")
-        private String userId; // 회원 아이디
+        @NotBlank(message = "조회할 채팅방의 회원 아이디를 입력해주세요.")
+        private String chatRoomMemberId; // 조회할 채팅방의 회원 아이디
 
-        @NotBlank(message = "관리자 아이디를 입력해주세요.")
-        private String adminId; // 관리자 아이디
+        @NotBlank(message = "조회하는 회원의 아이디를 입력해주세요.")
+        private String verifyMemberId; // 조회하는 회원의 아이디
     }
 
     @Getter
@@ -33,6 +32,6 @@ public class ChatRoomVerifyDto {
         private Long id; // pk
         private String adminId; // 관리자 아이디
         private String memberId; // 회원 아이디
-        private List<ChatMessageServiceDto> chatMessageList; // 채팅메시지
+        private long notReadMessage; // 안 읽은 메시지 갯수
     }
 }
