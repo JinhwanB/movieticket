8a744f6672b12e221a789c274e9ceb670f4b48de
jinhwan Bea <paewhal20@gmail.com>
2024-07-20 19:32:58 +0900

    Feat: 채팅메시지 레포지토리 메소드 추가
    채팅방을 통해 전체 채팅 메시지 리스트 조회하는 메소드 추가

--- src/main/java/com/jh/movieticket/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/jh/movieticket/chat/repository/ChatMessageRepository.java
@@ -1,6 +1,8 @@
 package com.jh.movieticket.chat.repository;
 
 import com.jh.movieticket.chat.domain.ChatMessage;
+import com.jh.movieticket.chat.domain.ChatRoom;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,6 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
     @Modifying
     @Query("delete ChatMessage cm where cm.sender.id = :parentId")
     void deleteChatMessageByMember(Long parentId); // 회원 소프트딜리트로 인한 채팅메시지 삭제 메소드
+
+    List<ChatMessage> findAllByChatRoom(ChatRoom chatRoom); // 채팅방을 통해 채팅 메시지 리스트 조회
 }
