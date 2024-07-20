4addf298cfe73cce0321c48cba404f5d18df5cf2
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-21 05:53:52 +0900

    Refactor: exceptionHandler 수정
    unique 제약 조건 위반 exception 핸들러와 채팅방 관련 exception 핸들러 추가

--- src/main/java/com/jh/movieticket/config/GlobalExceptionHandler.java
+++ src/main/java/com/jh/movieticket/config/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.jh.movieticket.config;
 
 import com.jh.movieticket.auth.TokenException;
+import com.jh.movieticket.chat.exception.ChatRoomException;
 import com.jh.movieticket.mail.exception.MailException;
 import com.jh.movieticket.member.exception.MemberException;
 import com.jh.movieticket.theater.exception.TheaterException;
@@ -10,6 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -91,11 +93,26 @@ public class GlobalExceptionHandler {
 
     // 필수 PathVariable 값 존재하지 않을 경우 에러 핸들러
     @ExceptionHandler(MissingPathVariableException.class)
-    private ResponseEntity<GlobalApiResponse<String>> handleMissingPathVariableException(MissingPathVariableException e){
+    private ResponseEntity<GlobalApiResponse<String>> handleMissingPathVariableException(
+        MissingPathVariableException e) {
 
         log.error("필수 PathVariable 값 존재하지 않음 exception = {}", e.getMessage());
 
-        return ResponseEntity.badRequest().body(GlobalApiResponse.toGlobalResponseFail(HttpStatus.BAD_REQUEST, "PathVariable 값은 필수값입니다."));
+        return ResponseEntity.badRequest().body(
+            GlobalApiResponse.toGlobalResponseFail(HttpStatus.BAD_REQUEST,
+                "PathVariable 값은 필수값입니다."));
+    }
+
+    // unique 제약 조건 위반 exception 핸들러
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    private ResponseEntity<GlobalApiResponse<String>> handleDataIntegrityViolationException(
+        DataIntegrityViolationException e) {
+
+        log.error("unique 제약 조건 위반", e);
+
+        return ResponseEntity.badRequest()
+            .body(GlobalApiResponse.toGlobalResponseFail(HttpStatus.BAD_REQUEST,
+                "unique 제약 조건에 위반된 요청입니다. 생성 또는 변경하려는 요청 중 중복된 값이 포함되어있습니다."));
     }
 
     // 회원 관련 에러 핸들러 -> 400 에러
@@ -140,5 +157,16 @@ public class GlobalExceptionHandler {
         return ResponseEntity.badRequest()
             .body(GlobalApiResponse.toGlobalResponseFail(HttpStatus.BAD_REQUEST,
                 e.getTheaterErrorCode().getMessage()));
+    }
+
+    // 채팅방 관련 에러 핸들러 -> 400 에러
+    @ExceptionHandler(ChatRoomException.class)
+    private ResponseEntity<GlobalApiResponse<?>> handleChatRoomException(ChatRoomException e) {
+
+        log.error("채팅방 관련 exception", e);
+
+        return ResponseEntity.badRequest()
+            .body(GlobalApiResponse.toGlobalResponseFail(HttpStatus.BAD_REQUEST,
+                e.getChatRoomErrorCode().getMessage()));
     }
 }
