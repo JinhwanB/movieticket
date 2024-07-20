e00e934ad4d1a63224be4a41b4f5ef2ef8f8b757
jinhwan Bea <paewhal20@gmail.com>
2024-07-20 19:33:17 +0900

    Feat: 채팅방 에러코드 추가

--- src/main/java/com/jh/movieticket/chat/exception/ChatRoomErrorCode.java
+++ src/main/java/com/jh/movieticket/chat/exception/ChatRoomErrorCode.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ChatRoomErrorCode {
 
+    FAIL_TO_OUT(HttpStatus.BAD_REQUEST.value(), "해당 채팅방에 입장한 회원이 아닙니다."),
+    FAIL_TO_ENTER(HttpStatus.BAD_REQUEST.value(), "해당 채팅방에 입장할 수 없는 회원입니다."),
+    EXIST_CHAT_ROOM(HttpStatus.BAD_REQUEST.value(), "이미 채팅방이 존재합니다."),
     NOT_FOUND_CHAT_ROOM(HttpStatus.BAD_REQUEST.value(), "해당 채팅방이 없습니다.");
 
     private final int status;
