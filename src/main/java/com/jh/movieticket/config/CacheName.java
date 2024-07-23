6b3401f2eda66544c4c9747dd6c97b904354707c
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-23 15:06:23 +0900

    Refactor: 캐시 이름 필드 추가
    예약 캐시 이름 추가

--- src/main/java/com/jh/movieticket/config/CacheName.java
+++ src/main/java/com/jh/movieticket/config/CacheName.java
@@ -6,4 +6,5 @@ public class CacheName {
     public static final String MOVIE_CACHE_NAME = "movie";
     public static final String THEATER_CACHE_NAME = "theater";
     public static final String CHAT_MESSAGE_CACHE_NAME = "chatMessage";
+    public static final String RESERVATION_CACHE_NAME = "reservation";
 }
