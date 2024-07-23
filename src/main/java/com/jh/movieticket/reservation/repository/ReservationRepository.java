65f6abc44d81e35f41e06240c0e01ba53d1fdf58
jinhwanB <123534245+JinhwanB@users.noreply.github.com>
2024-07-23 15:12:32 +0900

    Refactor: 예약 레포지토리 추가
    예약 번호 중복 여부 확인 메소드 추가
    
    회원 아이디를 통해 예약 리스트 페이징하여 조회하는 메소드 추가

--- src/main/java/com/jh/movieticket/reservation/repository/ReservationRepository.java
+++ src/main/java/com/jh/movieticket/reservation/repository/ReservationRepository.java
@@ -1,6 +1,8 @@
 package com.jh.movieticket.reservation.repository;
 
 import com.jh.movieticket.reservation.domain.Reservation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +14,10 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
     @Modifying
     @Query("DELETE Reservation r WHERE r.member.id = :parentId")
     void deleteReservationByMember(Long parentId); // 회원 소프트딜리트로 인한 예매 엔티티 삭제 메소드
+
+    boolean existsByReservationNumber(String reservationNumber); // 예약 번호 중복 여부 확인
+
+    @Query("select r from Reservation r where r.member.userId = :memberId and r.deleteDate is null")
+    Page<Reservation> findAllByMemberId(String memberId,
+        Pageable pageable); // 회원 아이디를 통해 예약 리스트 페이징하여 조회
 }
